d3fb5a0cd891314019c1bdc00af78821a0fc9f8f
Murilo Carvalho <MuriloCarvalho2583@Outlook.com>
2026-09-24 15:34:00 -0300

    feature: adds support banner, prdocut card and buttons for previous and last products

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,6 +7,8 @@ import category_options from './components/category_options'
 import SectionTitle from './components/related_products'
 import CategoryTabs from './components/tech_products_categories'
 import { PrevButton, NextButton } from './components/buttons'
+import ProductCard from './components/product_card'
+import support_banner from './components/support-banner'
 
 
 function App() {
@@ -21,15 +23,30 @@ function App() {
       <div style={{width:"100%", height:"15%", display:"flex",justifyContent:"center", marginBottom: "4%"}}>
         {category_options()}
       </div>
+
       {SectionTitle("Produtos Relacionados")}
       {CategoryTabs()}
 
-      <div style={{width:"100%", height:"15%", display:"flex", flexDirection:"row", justifyContent:"center", gap:"10%"}}>
+      <div style={{width:"100%", height:"auto", display:"flex", flexDirection:"row", 
+                   justifyContent:"center", alignItems:"center", gap:"10%", marginBottom:"5%"}}>
         {PrevButton()}
-        {}
+        {ProductCard("https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png","test",10)}
         {NextButton()}
       </div>
 
+       
+    
+      <div style={{width:"100%", height:"100%", display:"flex", flexDirection:"row", gap: "5%", justifyContent:"center" }}>
+
+          <div style={{width:"40%", height:"100%"}}>
+            {support_banner()}
+          </div>
+
+          <div style={{width:"35%", height:"100%"}}>
+            {support_banner()}
+          </div>
+
+      </div>
     </div>
    
   )
